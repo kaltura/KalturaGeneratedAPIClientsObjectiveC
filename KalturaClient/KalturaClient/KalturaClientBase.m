ce3d56a538ec923c66b403db1d94f99fc0400e3b
Kaltura CSI <community@kaltura.com>
2015-07-31 00:48:04 -0400

    New clientlib ver

--- KalturaClient/KalturaClient/KalturaClientBase.m
+++ KalturaClient/KalturaClient/KalturaClientBase.m
@@ -602,7 +602,7 @@ NSString* const KalturaClientErrorDomain = @"KalturaClientErrorDomain";
     if (self == nil)
         return nil;
 
-    self.clientTag = @"objCLib:15-07-30";
+    self.clientTag = @"objCLib:15-07-31";
     self.partnerId = -1;
     self.serviceUrl = @"http://www.kaltura.com";
     self.requestTimeout = 120;
