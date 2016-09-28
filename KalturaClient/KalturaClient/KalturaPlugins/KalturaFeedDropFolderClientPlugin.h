1d16d275da7fd91af16f2ce614005ede0bb4f919
Kaltura CSI <community@kaltura.com>
2016-09-28 01:02:19 -0400

    New clientlib ver

--- KalturaClient/KalturaClient/KalturaPlugins/KalturaFeedDropFolderClientPlugin.h
+++ KalturaClient/KalturaClient/KalturaPlugins/KalturaFeedDropFolderClientPlugin.h
@@ -31,6 +31,40 @@
 #import "KalturaDropFolderClientPlugin.h"
 
 ///////////////////////// enums /////////////////////////
+// @package Kaltura
+// @subpackage Client
+@interface KalturaFeedDropFolderFileOrderBy : NSObject
++ (NSString*)CREATED_AT_ASC;
++ (NSString*)FILE_NAME_ASC;
++ (NSString*)FILE_SIZE_ASC;
++ (NSString*)FILE_SIZE_LAST_SET_AT_ASC;
++ (NSString*)ID_ASC;
++ (NSString*)PARSED_FLAVOR_ASC;
++ (NSString*)PARSED_SLUG_ASC;
++ (NSString*)UPDATED_AT_ASC;
++ (NSString*)CREATED_AT_DESC;
++ (NSString*)FILE_NAME_DESC;
++ (NSString*)FILE_SIZE_DESC;
++ (NSString*)FILE_SIZE_LAST_SET_AT_DESC;
++ (NSString*)ID_DESC;
++ (NSString*)PARSED_FLAVOR_DESC;
++ (NSString*)PARSED_SLUG_DESC;
++ (NSString*)UPDATED_AT_DESC;
+@end
+
+// @package Kaltura
+// @subpackage Client
+@interface KalturaFeedDropFolderOrderBy : NSObject
++ (NSString*)CREATED_AT_ASC;
++ (NSString*)ID_ASC;
++ (NSString*)NAME_ASC;
++ (NSString*)UPDATED_AT_ASC;
++ (NSString*)CREATED_AT_DESC;
++ (NSString*)ID_DESC;
++ (NSString*)NAME_DESC;
++ (NSString*)UPDATED_AT_DESC;
+@end
+
 ///////////////////////// classes /////////////////////////
 // @package Kaltura
 // @subpackage Client
@@ -75,6 +109,26 @@
 @property (nonatomic,copy) NSString* feedXmlPath;
 - (KalturaFieldType)getTypeOfHash;
 - (KalturaFieldType)getTypeOfFeedXmlPath;
+@end
+
+// @package Kaltura
+// @subpackage Client
+@interface KalturaFeedDropFolderBaseFilter : KalturaDropFolderFilter
+@end
+
+// @package Kaltura
+// @subpackage Client
+@interface KalturaFeedDropFolderFileBaseFilter : KalturaDropFolderFileFilter
+@end
+
+// @package Kaltura
+// @subpackage Client
+@interface KalturaFeedDropFolderFileFilter : KalturaFeedDropFolderFileBaseFilter
+@end
+
+// @package Kaltura
+// @subpackage Client
+@interface KalturaFeedDropFolderFilter : KalturaFeedDropFolderBaseFilter
 @end
 
 ///////////////////////// services /////////////////////////
